fb122a65c9800880a9ec295dd65293b79390a214
LucasWaelti <31965233+LucasWaelti@users.noreply.github.com>
2018-01-02 23:19:06 +0100

    Update self_docking.py

--- self_docking/self_docking.py
+++ self_docking/self_docking.py
@@ -1,19 +1,40 @@
 # PATH to the file: C:\Users\Lucas Waelti\Documents\Cozmo\self_docking
 '''
     Written by @Luc, https://forums.anki.com/u/Luc/activity
+
+    This program enables Cozmo to drive off its charger, collect its cubes
+    by placing them near its charger and then climbing back onto its 
+    charger when it is done. Cozmo will directly try to climb back onto its charger
+    if the battery is low. No further help or markers are required. 
+    It is possible that Cozmo does not find a cube or its charger, 
+    so it will ask for help if needed. Just place the required object 
+    or Cozmo in a position where Cozmo can keep working and let it finish its job!
+
+	TODO (improvements): 
+	- Support already stacked or rolled cubes when cleaning up. 
+	- Stack cubes as a pyramid instead of current configuration. 
+	- Detect if backup_onto_charger() has succeeded (not possible?).
 '''
 
 import cozmo
 from cozmo.util import degrees, radians, distance_mm, speed_mmps
+from cozmo.objects import LightCube1Id, LightCube2Id, LightCube3Id
 
 import time
 import math
 
-robot = None
+# Choose on which side of the charger (when facing it),
+# Cozmo should put its cubes.
+# SIDE = 1 (left), SIDE = -1 (right)
+SIDE = 1
 
-def init_robot(cozmo_robot: cozmo.robot.Robot):
-    global robot
-    robot = cozmo_robot
+# Pitch value when head is horizonal (angle = 0)
+pitch_threshold = 0
+
+# Define how many search cycles must be engaged (5 sec each) while looking for cubes.
+# When this number is reached, Cozmo will stop looking for its cubes and will drive 
+# back onto its charger. 
+retries = 5
 
 ########################## Animations ##########################
 
@@ -27,7 +48,6 @@ def play_animation(robot: cozmo.robot.Robot, anim_trig, body = False, lift = Fal
                             ignore_body_track = body, ignore_head_track=False,
                             ignore_lift_track = lift).wait_for_completed()
 
-        
 def frustrated(robot: cozmo.robot.Robot):
     trigger = cozmo.anim.Triggers.FrustratedByFailureMajor  
     play_animation(robot,trigger)
@@ -36,19 +56,31 @@ def celebrate(robot: cozmo.robot.Robot):
     trigger = cozmo.anim.Triggers.CodeLabCelebrate  
     play_animation(robot,trigger,body=True,lift=True,para=True)
 
-##########################Cubes related code##########################
+########################## Cube lights related code ##########################
+
+def switch_cube_on(cube: cozmo.objects.LightCube):
+	cube.set_lights(cozmo.lights.green_light)
+	return
+
+def switch_cube_off(cube: cozmo.objects.LightCube):
+	cube.set_lights_off()
+	return
+
+########################## Cubes related code ##########################
 
 # Cozmo's memory to know which cube still 
 # needs to be put next to the charger
 cubeIDs = []
+cubes = []
 
 def look_for_next_cube():
     # look for a cube that was not put next to the charger yet
-    global robot, cubeIDs
-    
+    global robot, cubeIDs, cubes, retries
+
+    i = 0
     valid = True
     while(True):
-        
+        i += 1
         behavior = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
         try: 
             seen_cube = robot.world.wait_for_observed_light_cube(timeout=5,include_existing=True)
@@ -58,11 +90,13 @@ def look_for_next_cube():
         
         if(seen_cube == None):
             frustrated(robot)
+            robot.say_text('Cube?',duration_scalar=0.5).wait_for_completed()
         else:
             # A cube was seen but we have to know if it has to be moved
             #print(seen_cube)
             if(len(cubeIDs) == 0):
                 cubeIDs.append(seen_cube.object_id)
+                cubes.append(seen_cube)
                 return seen_cube # communicate that this cube has to be moved
             else:
                 for i in range(0,len(cubeIDs)):
@@ -70,47 +104,115 @@ def look_for_next_cube():
                         valid = False # that cube was already taken care of
                 if(valid):
                     cubeIDs.append(seen_cube.object_id)
+                    cubes.append(seen_cube)
                     return seen_cube
                 else:
                     valid = True
-    return None
+
+        if(i >= retries):
+        	print('ERROR: Aborting clean up, Cozmo cannot find all its cubes.')
+        	break
+    return 0
 
 def pickUp_cube(cube: cozmo.objects.LightCube):
     global robot
-    action = robot.pickup_object(cube,use_pre_dock_pose=True, in_parallel=False, num_retries=2)
+    action = robot.pickup_object(cube,use_pre_dock_pose=True, in_parallel=False, num_retries=5)
     action.wait_for_completed()
-    return
+    if(action.has_failed):
+    	print('ERROR: Picking cube ' + str(cube.cube_id) + ' has failed.')
+    	print('MESSAGE: Aborting clean up, engaging docking procedure.')
+    	return 0
+    return 1
 def putDown_cube(cube: cozmo.objects.LightCube):
     global robot
-    action = robot.place_object_on_ground_here(cube, in_parallel=False, num_retries=0)
+    action = robot.place_object_on_ground_here(cube, in_parallel=False, num_retries=2)
     action.wait_for_completed()
-    return
+    return 1
 def stack_cube(target: cozmo.objects.LightCube):
     global robot
-    action = robot.place_on_object(target, in_parallel=False, num_retries=0)
+    action = robot.place_on_object(target, in_parallel=False, num_retries=2)
     action.wait_for_completed()
-    return 
+    return 1
 
 def clean_up_cubes():
-    global cubeIDs
+    global cubeIDs,SIDE
 
     cubeIDs = []
-    charger = find_charger()
-    print('Charger located')
+    switch_cube_off(robot.world.get_light_cube(LightCube1Id))
+    switch_cube_off(robot.world.get_light_cube(LightCube2Id))
+    switch_cube_off(robot.world.get_light_cube(LightCube3Id))
+
     go_to_charger()
     turn_around()
     
+    # I. Find first cube and put it next to charger
     cube = look_for_next_cube()
+    if not cube:
+    	return
+    switch_cube_on(cube)
     print(cubeIDs)
-    pickUp_cube(cube)
+    success = pickUp_cube(cube)
+    if not success:
+    	switch_cube_off(cube) 
+    	return
+    charger = go_to_charger()
+    final_adjust(charger,60)
+    robot.turn_in_place(degrees(SIDE*60)).wait_for_completed()
+    robot.drive_straight(distance_mm(100),speed_mmps(40)).wait_for_completed()
     putDown_cube(cube)
+    switch_cube_off(cube)
+    # Get back in front of charger
+    robot.drive_straight(distance_mm(-70),speed_mmps(40)).wait_for_completed()
+    robot.turn_in_place(degrees(SIDE*180-SIDE*60)).wait_for_completed()
+    robot.drive_straight(distance_mm(40),speed_mmps(40)).wait_for_completed()
 
-    while(len(cubeIDs) < 3):
-        look_for_next_cube()
-        print(cubeIDs)
+    # II. Find next cube and stack on first placed cube
+    cube = look_for_next_cube()
+    if(cube == None):
+    	return
+    switch_cube_on(cube)
+    print(cubeIDs)
+    success = pickUp_cube(cube)
+    if not success:
+    	switch_cube_off(cube)
+    	return
+    charger = go_to_charger()
+    robot.turn_in_place(degrees(SIDE*70)).wait_for_completed() #SIDE*180
+    stack_cube(cubes[0])
+    switch_cube_off(cube)
+    # Get back in front of charger
+    #robot.drive_straight(distance_mm(-70),speed_mmps(40)).wait_for_completed()
+    charger = go_to_charger()
+    final_adjust(charger,100,80)
+    robot.turn_in_place(degrees(SIDE*180)).wait_for_completed() #-SIDE*70
+    robot.set_lift_height(height=0,max_speed=10,in_parallel=False).wait_for_completed()
+    #robot.drive_straight(distance_mm(40),speed_mmps(40)).wait_for_completed()
+
+    # III. Go get the last cube and lay it in front of the others
+    cube = look_for_next_cube()
+    if(cube == None):
+    	return
+    switch_cube_on(cube)
+    print(cubeIDs)
+    success = pickUp_cube(cube)
+    if not success:
+    	switch_cube_off(cube)
+    	return
+    turn_around()
+    charger = go_to_charger()
+    final_adjust(charger,80)
+    robot.turn_in_place(degrees(SIDE*60)).wait_for_completed()
+    robot.go_to_object(cubes[0], distance_mm(120), in_parallel=False, num_retries=1).wait_for_completed()
+    putDown_cube(cube)
+    switch_cube_off(cube)
+    # Get back in front of charger
+    robot.drive_straight(distance_mm(-40),speed_mmps(40)).wait_for_completed()
+    robot.turn_in_place(degrees(SIDE*180-SIDE*60)).wait_for_completed()
+    robot.drive_straight(distance_mm(50),speed_mmps(40)).wait_for_completed()
+    turn_around()
     return
 
-##########################Charger related code##########################
+########################## Charger related code ##########################
 
 def find_charger():
     global robot
@@ -151,6 +253,7 @@ def go_to_charger():
         charger = find_charger()
     
     action = robot.go_to_object(charger,distance_from_object=distance_mm(80), in_parallel=False, num_retries=5)
+    #action = robot.go_to_pose(charger.pose)
     action.wait_for_completed()
     return charger
 
@@ -175,22 +278,33 @@ def disp_coord(charger: cozmo.objects.Charger):
 
 PI = 3.14159265359
 def clip_angle(angle=3.1415):
-    # Retreive supplementary turns that aren't useful (in radians)
     global PI
-    if(angle >= 2*PI):
-        while(angle >= 2*PI):
-            angle -= 2*PI
-        return angle
-    elif(angle <= -2*PI):
-        while(angle <= -2*PI):
-            angle += 2*PI
-        return angle
+
+    # Retreive supplementary turns (in radians)
+    while(angle >= 2*PI):
+        angle -= 2*PI
+    while(angle <= -2*PI):
+        angle += 2*PI
+    # Select shortest rotation to reach the target
+    if(angle > PI):
+    	angle -= 2*PI
+    elif(angle < -PI):
+    	angle += 2*PI
     return angle
 
-def final_adjust(charger: cozmo.objects.Charger):
-    # Final adjustement to properly face the charger
+def check_tol(charger: cozmo.objects.Charger,dist_charger=40):
+    # Check if the position tolerance in front of the charger is respected
     global robot,PI
 
+    distance_tol = 5 # mm, tolerance for placement error
+    angle_tol = 5*PI/180 # rad, tolerance for orientation error
+
+    try: 
+        charger = robot.world.wait_for_observed_charger(timeout=2,include_existing=True)
+    except:
+        print('WARNING: Cannot see the charger to verify the position.')
+
+    # Calculate positions
     r_coord = [0,0,0]
     c_coord = [0,0,0]
     # Coordonates of robot and charger
@@ -204,26 +318,66 @@ def final_adjust(charger: cozmo.objects.Charger):
     c_zRot = charger.pose.rotation.angle_z.radians
 
     # Create target position 
-    dist_charger = 40 # mm, distance if front of charger
+    # dist_charger in mm, distance if front of charger
     c_coord[0] -=  dist_charger*math.cos(c_zRot)
     c_coord[1] -=  dist_charger*math.sin(c_zRot)
 
     # Direction and distance to target position (in front of charger)
     distance = math.sqrt((c_coord[0]-r_coord[0])**2 + (c_coord[1]-r_coord[1])**2 + (c_coord[2]-r_coord[2])**2)
-    vect = [c_coord[0]-r_coord[0],c_coord[1]-r_coord[1],c_coord[2]-r_coord[2]]
-    # Angle of vector going from robot's origin to target's position
-    theta_t = math.atan2(vect[1],vect[0])
 
-    print('CHECK: Adjusting position')
-    # Face the target position
-    angle = clip_angle((theta_t-r_zRot))
-    robot.turn_in_place(radians(angle)).wait_for_completed()
-    # Drive toward the target position
-    robot.drive_straight(distance_mm(distance),speed_mmps(20)).wait_for_completed()
-    # Face the charger
-    angle = clip_angle((c_zRot-theta_t))
-    robot.turn_in_place(radians(angle)).wait_for_completed()
-    print('CHECK: Robot aligned')
+    if(distance < distance_tol and math.fabs(r_zRot-c_zRot) < angle_tol):
+    	return 1
+    else: 
+    	return 0
+
+def final_adjust(charger: cozmo.objects.Charger,dist_charger=40,speed=20,critical=False):
+    # Final adjustement to properly face the charger.
+    # The position can be adjusted several times if 
+    # the precision is critical, i.e. when climbing
+    # back onto the charger.  
+    global robot,PI
+
+    while(True):
+        # Calculate positions
+	    r_coord = [0,0,0]
+	    c_coord = [0,0,0]
+	    # Coordonates of robot and charger
+	    r_coord[0] = robot.pose.position.x #.x .y .z, .rotation otherwise
+	    r_coord[1] = robot.pose.position.y
+	    r_coord[2] = robot.pose.position.z
+	    r_zRot = robot.pose_angle.radians # .degrees or .radians
+	    c_coord[0] = charger.pose.position.x
+	    c_coord[1] = charger.pose.position.y
+	    c_coord[2] = charger.pose.position.z
+	    c_zRot = charger.pose.rotation.angle_z.radians
+
+	    # Create target position 
+	    # dist_charger in mm, distance if front of charger
+	    c_coord[0] -=  dist_charger*math.cos(c_zRot)
+	    c_coord[1] -=  dist_charger*math.sin(c_zRot)
+
+	    # Direction and distance to target position (in front of charger)
+	    distance = math.sqrt((c_coord[0]-r_coord[0])**2 + (c_coord[1]-r_coord[1])**2 + (c_coord[2]-r_coord[2])**2)
+	    vect = [c_coord[0]-r_coord[0],c_coord[1]-r_coord[1],c_coord[2]-r_coord[2]]
+	    # Angle of vector going from robot's origin to target's position
+	    theta_t = math.atan2(vect[1],vect[0])
+
+	    print('CHECK: Adjusting position')
+	    # Face the target position
+	    angle = clip_angle((theta_t-r_zRot))
+	    robot.turn_in_place(radians(angle)).wait_for_completed()
+	    # Drive toward the target position
+	    robot.drive_straight(distance_mm(distance),speed_mmps(speed)).wait_for_completed()
+	    # Face the charger
+	    angle = clip_angle((c_zRot-theta_t))
+	    robot.turn_in_place(radians(angle)).wait_for_completed()
+
+        # In case the robot does not need to climb onto the charger
+	    if not critical:
+	        break
+	    elif(check_tol(charger,dist_charger)):
+	    	print('CHECK: Robot aligned relativ to the charger.')
+	    	break
     return
 
 def restart_procedure(charger: cozmo.objects.Charger):
@@ -233,7 +387,7 @@ def restart_procedure(charger: cozmo.objects.Charger):
     robot.set_lift_height(height=0.5,max_speed=10,in_parallel=True).wait_for_completed()
     robot.pose.invalidate()
     charger.pose.invalidate()
-    print('Driving away')
+    print('ABORT: Driving away')
     #robot.drive_straight(distance_mm(150),speed_mmps(80),in_parallel=False).wait_for_completed()
     robot.drive_wheels(80,80,duration=2)
     turn_around()
@@ -243,35 +397,50 @@ def restart_procedure(charger: cozmo.objects.Charger):
     return
 
 def get_on_charger():
-    global robot
+    global robot,pitch_threshold
 
+    robot.set_head_angle(degrees(0),in_parallel=False).wait_for_completed()
+    pitch_threshold = math.fabs(robot.pose_pitch.degrees)
+    pitch_threshold += 1 # Add 1 degree to threshold
+    print('Pitch threshold: ' + str(pitch_threshold))
+
+    # Let Cozmo first look for the charger once again. The coordinates
+    # tend to be too unprecise if an old coordinates system is kept.
+    if robot.world.charger is not None and robot.world.charger.pose.is_comparable(robot.pose):
+    	robot.world.charger.pose.invalidate()
+
+    # Head towards charger
     charger = go_to_charger()
 
-    final_adjust(charger)
+    # Adjust position in front of the charger
+    final_adjust(charger,critical=True)
     
+    # Turn around and start going backward
     turn_around()
-    robot.drive_wheel_motors(-100,-100)
+    robot.drive_wheel_motors(-120,-120)
     robot.set_lift_height(height=0.5,max_speed=10,in_parallel=True).wait_for_completed()
     robot.set_head_angle(degrees(0),in_parallel=True).wait_for_completed()
 
-    timeout = 2 # seconds before timeout
+    # This section allow to wait for Cozmo to arrive on its charger
+    # and detect eventual errors. The whole procedure will be restarted
+    # in case something goes wrong.
+    timeout = 1 # seconds before timeout
     t = 0
-    # Wait for backwheels to climb on charger
+    # Wait for back wheels to climb on charger
     while(True):
-        time.sleep(.5)
-        t += 0.5
+        time.sleep(.1)
+        t += 0.1
         if(t >= timeout):
             print('ERROR: robot timed out before climbing on charger.')
             restart_procedure(charger)
             return
-        elif(math.fabs(robot.pose_pitch.degrees) > 1.7):
+        elif(math.fabs(robot.pose_pitch.degrees) >= pitch_threshold):
             print('CHECK: backwheels on charger.')
             break
-        
     # Wait for front wheels to climb on charger
+    timeout = 2
     t = 0
     while(True):
-        # print(robot.pose_pitch.degrees)
         time.sleep(.1)
         t += 0.1
         if(math.fabs(robot.pose_pitch.degrees) > 20 or t >= timeout):
@@ -279,35 +448,65 @@ def get_on_charger():
             print('ERROR: robot climbed on charger\'s wall or timed out.')
             restart_procedure(charger)
             return
-        elif(math.fabs(robot.pose_pitch.degrees) < 1.7):
+        elif(math.fabs(robot.pose_pitch.degrees) < pitch_threshold):
             print('CHECK: robot on charger, backing up on pins.')
             robot.stop_all_motors()
             break
 
+    # Final backup onto charger's contacts
     robot.set_lift_height(height=0,max_speed=100,in_parallel=True).wait_for_completed()
-    robot.backup_onto_charger(max_drive_time=3)
+    action = robot.backup_onto_charger(max_drive_time=3)
     print('PROCEDURE SUCCEEDED')
 
     # Celebrate success
     robot.drive_off_charger_contacts().wait_for_completed()
-    celebrate(robot)
+    celebrate(robot) # A small celebration where only the head moves
     robot.backup_onto_charger(max_drive_time=3)
     return
+
+########################## Battery related code ##########################
+
+def check_battery():
+	global robot
+
+	if(robot.battery_voltage < 3.5):
+		print('Battery is low, directly docking to charger without cleaning up.')
+		return 1
+	else:
+		return 0
 
 ########################## General code ##########################
 
 def turn_around():
     global robot
-    robot.turn_in_place(degrees(180)).wait_for_completed()
+    robot.turn_in_place(degrees(-178)).wait_for_completed()
     return
 
 ########################## Main ##########################
 
-def cozmo_program(cozmo_robot: cozmo.robot.Robot):
-    global robot, cubeIDs
-    init_robot(cozmo_robot)
-    
+robot = None
+
+def init_robot(cozmo_robot: cozmo.robot.Robot):
+    global robot
+    robot = cozmo_robot
+
+def execute_procedure():
+    low_battery = check_battery()
+    # HACK HACK HACK
+    #low_battery = 1
+    if not low_battery:
+        clean_up_cubes()
     get_on_charger()
+    return
+
+def cozmo_program(cozmo_robot: cozmo.robot.Robot):
+    global robot
+    init_robot(cozmo_robot)
+
+    # Get off charger
+    robot.drive_straight(distance_mm(110),speed_mmps(80)).wait_for_completed()
+
+    execute_procedure()
     return
 
 if __name__ == "__main__":
